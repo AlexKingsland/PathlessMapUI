bb11a9ad78117f3cb6dc52b83c4e5a1188aee190
AlexKingsland <alex.w.king@hotmail.com>
2025-03-06 08:17:05 -0800

    Update user registration form flow to include image and alias

--- src/components/auth/RegisterPage.jsx
+++ src/components/auth/RegisterPage.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "../../css/auth/AuthPage.css";
 
@@ -11,24 +10,72 @@ function RegisterPage() {
   const [role, setRole] = useState("traveler");
   const [name, setName] = useState("");
   const [bio, setBio] = useState("");
+  const [image, setImage] = useState("");
+  const [alias, setAlias] = useState("");
   const [error, setError] = useState("");
+  const [imageError, setImageError] = useState("");
+  const [imagePreview, setImagePreview] = useState(null);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault(); // Prevents form from reloading the page
 
+    const formData = new FormData();
+    formData.append("email", email);
+    formData.append("password", password);
+    formData.append("role", role);
+    formData.append("name", name);
+    formData.append("bio", bio);
+    formData.append("alias", alias);
+    if (image) {
+      formData.append("profile_image", image);
+    }
+
     try {
-      await axios.post(`${baseURL}/auth/register`, {
-        email,
-        password,
-        role,
-        name,
-        bio,
+      const response = await fetch(`${baseURL}/auth/register`, {
+        method: "POST",
+        body: formData,
       });
+  
+      const data = await response.json();
+  
+      if (!response.ok) {
+        throw new Error(data.error || "Registration failed. Please try again.");
+      }
+  
       setError("");
       navigate("/login"); // Redirect to login page only after successful registration
     } catch (err) {
-      setError("Registration failed. Please try again.");
+      setError(err.message);
+    }
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      if (file.size > 2 * 1024 * 1024) {
+        setImageError("File size must be less than 2MB.");
+        setImage(null);
+        setImagePreview(null); // Clear preview on error
+        return;
+      }
+
+      if (!["image/jpeg", "image/png"].includes(file.type)) {
+        setImageError("Invalid file type. Only JPEG and PNG are allowed.");
+        setImage(null);
+        setImagePreview(null); // Clear preview on error
+        return;
+      }
+
+      setImageError("");
+      setImage(file);
+
+      // Create a file reader to generate the image preview
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImagePreview(reader.result);
+      };
+      reader.readAsDataURL(file);
     }
   };
 
@@ -37,6 +84,34 @@ function RegisterPage() {
       <div className="register-box">
         <h2>Become a Pathless User!</h2>
         <form onSubmit={handleSubmit}>
+          <div className="image-upload">
+            <input
+              type="file"
+              id="profile-image"
+              accept="image/png, image/jpeg"
+              onChange={handleImageChange}
+              style={{ display: "none" }} // Hide default input
+            />
+            <label htmlFor="profile-image" className="custom-file-button">
+              {imagePreview ? "Change Profile Picture" : "Upload Profile Picture"}
+            </label>
+
+            {imageError && <p className="error-text">{imageError}</p>}
+
+            {/* Display the image preview if available */}
+            {imagePreview && (
+              <div className="image-preview">
+                <img src={imagePreview} alt="Preview" />
+              </div>
+            )}
+          </div>
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="register-input"
+          />
           <input
             type="email"
             placeholder="Email"
@@ -53,20 +128,12 @@ function RegisterPage() {
             required
             className="register-input"
           />
-          <select
-            value={role}
-            onChange={(e) => setRole(e.target.value)}
-            required
-            className="register-input"
-          >
-            <option value="traveler">Traveler</option>
-            <option value="creator">Creator</option>
-          </select>
           <input
             type="text"
-            placeholder="Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            placeholder="Alias"
+            value={alias}
+            onChange={(e) => setAlias(e.target.value)}
+            required
             className="register-input"
           />
           <textarea
